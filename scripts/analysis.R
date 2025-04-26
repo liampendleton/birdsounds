3a3053e5bc140fed10fed00591f32ebb897e35f6
Liam Pendleton <liamcpendleton@gmail.com>
2025-04-25 18:20:53 -0700

    idk

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -4,8 +4,6 @@ library(dplyr)
 source(here("Scripts", "processing_function.R"))
 source(here("Scripts", "data_prep.R"))
 
-# Now you can call your function
 mfcc_data <- process_wav(segments)
 
-# Check it out
 head(mfcc_data)
