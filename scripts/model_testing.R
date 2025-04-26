3d07c542f5eef875dac52a0558dbfd92c76640d7
Liam Pendleton <liamcpendleton@gmail.com>
2025-04-25 19:54:02 -0700

    model works great with the four 5-sec clips. Next step is to use a new clip of one of the four species and see how it works!

--- scripts/model_testing.R
+++ scripts/model_testing.R
@@ -1,1 +1,7 @@
-asdf
+library(here)
+library(tuneR)
+library(dplyr)
+
+source(here("Scripts", "processing_function.R"))
+
+birdsound_model <- readRDS(here("data", "output", "birdsound_model.rds"))
--- scripts/model_training.R
+++ scripts/model_training.R
@@ -28,16 +28,28 @@ test_data <- anti_join(mfcc_data, train_data, by = c("MFCC_1", "MFCC_2", "MFCC_3
                                                      "MFCC_6", "MFCC_7", "MFCC_8", "MFCC_9", "MFCC_10",
                                                      "MFCC_11", "MFCC_12", "MFCC_13", "species"))
 
-#Begin training model. Species is a function of all MFCCs (species is a function of distinct sound features)
+#Train the model. Species is a function of all MFCCs (species is a function of distinct sound features)
 birdsound_model <- randomForest(species ~ MFCC_1 + MFCC_2 + MFCC_3 + MFCC_4 + MFCC_5 + 
                            MFCC_6 + MFCC_7 + MFCC_8 + MFCC_9 + MFCC_10 + 
                            MFCC_11 + MFCC_12 + MFCC_13, 
                          data = train_data, 
                          importance = TRUE)
 
+#How did things work?
+preds <- predict(birdsound_model, test_data) #predictions based on test data
+conf_matrix <- table(preds, test_data$species) #see how well it performed
+print(conf_matrix)
+
+#Calculate accuracy
+sum(diag(conf_matrix)) / sum(conf_matrix)
+
+#Assess variable importance
+importance(birdsound_model)
+varImpPlot(birdsound_model)
+
+#Look at the importance output. MFCC1 and 2 have the highest MDA and MDGini. The first one is, how much does accuracy drop when this variable is permuted, and honestly idk yet what the other one really means. I'll get back to that
+#Moving forward maybe I can consider fewer MFCCs to simplify things and potentially improve model performance
+
 print(birdsound_model) #Note that rows in the confusion matrix are true, while columns are model predictions
 saveRDS(birdsound_model, file = here("data", "output", "birdsound_model.rds"))
-
-
-
                                                      
